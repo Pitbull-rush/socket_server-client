a3a83c225de3e0fe0595e5b4facc04f1156c0d36
Nazarenko Maksim <m.n4zarenko@yandex.ru>
2025-08-12 22:45:16 +0300

    Add files via upload

---
+++ SocketClient/SocketClient.cpp
@@ -0,0 +1,86 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+
+    ADDRINFO hints{};
+    ADDRINFO* addrResult = nullptr;
+
+    hints.ai_family = AF_INET;       
+    hints.ai_socktype = SOCK_STREAM; 
+    hints.ai_protocol = IPPROTO_TCP; 
+
+    const char* serverAddress = "127.0.0.1"; 
+    const char* serverPort = "7777";         
+
+    if (getaddrinfo(serverAddress, serverPort, &hints, &addrResult) != 0) {
+        std::cerr << "getaddrinfo failed\n";
+        WSACleanup();
+        return 1;
+    }
+
+    SOCKET ConnectSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+    if (ConnectSocket == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed\n";
+        freeaddrinfo(addrResult);
+        WSACleanup();
+        return 1;
+    }
+
+    if (connect(ConnectSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen) == SOCKET_ERROR) {
+        std::cerr << "Unable to connect to server\n";
+        closesocket(ConnectSocket);
+        freeaddrinfo(addrResult);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(addrResult);
+
+    std::string sendBuffer;
+    char recvBuffer[512];
+
+    while (true) {
+        std::cout << "Enter message (exit to quit): ";
+        std::getline(std::cin, sendBuffer);
+
+        if (sendBuffer == "exit")
+            break;
+
+        int bytesSent = send(ConnectSocket, sendBuffer.c_str(), (int)sendBuffer.size(), 0);
+        if (bytesSent == SOCKET_ERROR) {
+            std::cerr << "Send failed\n";
+            break;
+        }
+
+        int bytesRecv = recv(ConnectSocket, recvBuffer, sizeof(recvBuffer) - 1, 0);
+        if (bytesRecv > 0) {
+            recvBuffer[bytesRecv] = '\0';
+            std::cout << "Server response: " << recvBuffer << "\n";
+        }
+        else if (bytesRecv == 0) {
+            std::cout << "Connection closed by server\n";
+            break;
+        }
+        else {
+            std::cerr << "Recv failed\n";
+            break;
+        }
+    }
+
+    closesocket(ConnectSocket);
+    WSACleanup();
+    return 0;
+}
---
+++ SocketsServer/SocketsServer.cpp
@@ -0,0 +1,109 @@
+﻿#define WIN32_LEAN_AND_MEAN
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main() {
+    WSADATA wsaData{};
+    addrinfo hints{}, * addrResult = nullptr;
+    SOCKET listenSocket = INVALID_SOCKET;
+
+    const int RECV_BUF_SIZE = 512;
+    char recvBuffer[RECV_BUF_SIZE + 1];
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
+    }
+
+    ZeroMemory(&hints, sizeof(hints));
+    hints.ai_family = AF_INET;       
+    hints.ai_socktype = SOCK_STREAM;  
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;     
+
+    if (getaddrinfo(NULL, "7777", &hints, &addrResult) != 0) {
+        std::cerr << "getaddrinfo failed: " << WSAGetLastError() << "\n";
+        WSACleanup();
+        return 1;
+    }
+
+    listenSocket = socket(addrResult->ai_family, addrResult->ai_socktype, addrResult->ai_protocol);
+    if (listenSocket == INVALID_SOCKET) {
+        std::cerr << "socket failed: " << WSAGetLastError() << "\n";
+        freeaddrinfo(addrResult);
+        WSACleanup();
+        return 1;
+    }
+
+    if (bind(listenSocket, addrResult->ai_addr, (int)addrResult->ai_addrlen) == SOCKET_ERROR) {
+        std::cerr << "bind failed: " << WSAGetLastError() << "\n";
+        freeaddrinfo(addrResult);
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    freeaddrinfo(addrResult);
+
+    if (listen(listenSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << "listen failed: " << WSAGetLastError() << "\n";
+        closesocket(listenSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server is listening on port 7777...\n";
+
+    while (true) {
+        SOCKET clientSocket = accept(listenSocket, NULL, NULL);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << "accept failed: " << WSAGetLastError() << "\n";
+            continue;
+        }
+
+        std::cout << "Client connected.\n";
+
+        while (true) {
+            ZeroMemory(recvBuffer, sizeof(recvBuffer));
+            int bytesReceived = recv(clientSocket, recvBuffer, RECV_BUF_SIZE, 0);
+
+            if (bytesReceived > 0) {
+                if (bytesReceived > RECV_BUF_SIZE) bytesReceived = RECV_BUF_SIZE;
+                recvBuffer[bytesReceived] = '\0';
+
+                std::string received = recvBuffer;
+                std::cout << "Received (" << bytesReceived << "): " << received << "\n";
+
+                if (received == "exit") {
+                    std::cout << "Client requested disconnect (exit).\n";
+                    break;
+                }
+
+                std::string reply = "Server: " + received;
+                int bytesSent = send(clientSocket, reply.c_str(), (int)reply.size(), 0);
+                if (bytesSent == SOCKET_ERROR) {
+                    std::cerr << "send failed: " << WSAGetLastError() << "\n";
+                    break;
+                }
+            }
+            else if (bytesReceived == 0) {
+                std::cout << "Client disconnected gracefully.\n";
+                break;
+            }
+            else { 
+                std::cerr << "recv failed: " << WSAGetLastError() << "\n";
+                break;
+            }
+        } 
+
+        closesocket(clientSocket);
+        std::cout << "Client socket closed. Waiting for new connections...\n";
+    } 
+    closesocket(listenSocket);
+    WSACleanup();
+    return 0;
+}
